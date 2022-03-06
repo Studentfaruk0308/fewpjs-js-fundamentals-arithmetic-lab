2a6cc2814151d1474ae777664a9119c6041d055a
Studentfaruk0308 <studentfaruk0308@gmail.com>
2022-03-06 23:02:43 +1100

    fewpjs-js-fundamentals-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+let 
+    num1 = 31, 
+    num2 = 2, 
+    multiply = num1 * num2,
+
+    random = Math.floor(Math.random() + 1),
+
+    num3 = 14,
+    num4 = 5,
+    mod = num3 % num4,
+    max = Math.max(5, 10, 15, 20)
+
+
+
+
